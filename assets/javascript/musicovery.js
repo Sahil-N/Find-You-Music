f9aa42546fdfcf8fb0e2b958e0da8486cfec250c
Emily Lau <emilylau5@gmail.com>
2017-10-06 12:55:10 -0700

    css changes

--- assets/javascript/musicovery.js
+++ assets/javascript/musicovery.js
@@ -2,7 +2,7 @@
 $("#find-artists").on("click", function(){
   $("#artist-info").empty();
   $("#similar-artist-info").empty();
-  var artistSearch = $(".form-control").val();
+  var artistSearch = $(".form-control").val().trim();
   var queryURL = "https://cors-anywhere.herokuapp.com/http://musicovery.com/api/V4/artist.php?fct=search&artistname=" + artistSearch + "&format=json"
   console.log(artistSearch)
 var simArtist;
@@ -24,9 +24,10 @@ $.ajax({
   var country = $("<p>").addClass("country-class").append("Country: " + artistCountry);
   console.log(genre)
   console.log(country)
-  $("#artist-info").append("<h3>" + artistName + "</h3>")
-  $("#artist-info").append(genre)
-  $("#artist-info").append(country)
+  $("#artist-info").addClass("col-md-3");
+  $("#artist-info").append("<h3>" + artistName + "</h3>");
+  $("#artist-info").append(genre);
+  $("#artist-info").append(country);
   callit();
 })
  function callit() {
@@ -45,9 +46,10 @@ $.ajax({
      artistCountry = JSON.parse(newResponse).root.artists.artist[i].country;
      var simGenre = $("<p>").addClass("genre-class").append("Genre: " + artistGenre);
      var simCountry = $("<p>").addClass("country-class").append("Country: " + artistCountry);
+    $("#similar-artist-info").addClass("col-md-3");
     $("#similar-artist-info").append("<h3>" + simName + "</h3>");
     $("#similar-artist-info").append(simGenre);
-    $("#similar-artist-info").append(simCountry)
+    $("#similar-artist-info").append(simCountry);
   }
   })
 }
