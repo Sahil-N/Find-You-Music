b0ffc2bb64f55e01814953edc92bd3258a05a778
Legion <xdjlee@gmail.com>
2017-10-06 13:30:10 -0700

    throw yt api into function

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,15 +1,21 @@
-var ytapikey = "AIzaSyB0WhHsY5aNb5nP0FA-KmWW7baChjnkK_I";
-var search = "kanye";
-search = search + " music -channel";
-var queryURL = "https://www.googleapis.com/youtube/v3/search?q=" + search + "&key=" + ytapikey + "&part=snippet"
+function ytSearch(search) {
+  var ytVids = [];//will hold videos later
+  var ytapikey = "AIzaSyB0WhHsY5aNb5nP0FA-KmWW7baChjnkK_I";
+  console.log(search);
+  var ytqueryURL = "https://www.googleapis.com/youtube/v3/search?q=" + search + "&key=" + ytapikey + "&part=snippet&type=video"
+  //google api link + search term + api key + snippet parameter + video only
+  $.ajax({
+    url: ytqueryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response.items)
+    for (var i = 0; i < 3; i++) {
+      ytVids.push("https://www.youtube.com/watch?v=" + response.items[i].id.videoId);
+    }
+    console.log(ytVids);
+  })
+  return ytVids;//returns array of 3 strings of video links
+}
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).done(function(response) {
-  console.log(response.items)
-
-  for (var i = 0; i < 3; i++) {
-    console.log("https://www.youtube.com/watch?v=" + response.items[i].id.videoId);
-  }
-})
+ytSearch("porter robinson");
+ytSearch("madeon");
