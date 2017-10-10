2ec77523b4440d3b1399844656e308021277e536
FEEDKurumu <xdjlee@gmail.com>
2017-10-10 14:19:47 -0700

    cleaned js

--- javascript.js
+++ javascript.js
@@ -13,7 +13,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var intervalId;
 var artistId;
 var login;
 
@@ -128,7 +127,7 @@ function getArtist(name) {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response){                                                                                                                                                                                                                                                               
+  }).done(function(response){
     artistId = JSON.parse(response).root.artists.artist.mbid;
     var artistName = JSON.parse(response).root.artists.artist.name;
     var artistGenre = JSON.parse(response).root.artists.artist.genre;
@@ -137,7 +136,7 @@ function getArtist(name) {
     var country = $("<p>").addClass("country-class").append("Country: " + artistCountry);
     $("#artist-info").addClass("col-md-3");
     $("#artist-info").append("<h3>" + artistName + "</h3>", genre, country);
-    intervalId = setTimeout(getSimilar, 1001);
+    setTimeout(getSimilar, 1001);
     ytSearch(name);
   })
 }
@@ -161,8 +160,8 @@ $("#loginbutton").on("click", function() {
 })
 
 $("#history").on("click", ".rendered", function() {
-  var artist = $(this).attr("data-name");
   clearStuff();
+  var artist = $(this).attr("data-name");
   getArtist(artist);
 })
 
